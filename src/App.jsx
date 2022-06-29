ecc5529fd3fafc07af194ac7c604a5f098850b4c
Vishwajeet <singhrathorev21@gmail.com>
2022-06-29 16:48:56 +0530

    congrats! updated

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const app = () => (
+  <>
+    {/*react.fragments or div upr wale arrow me*/}
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.hiii hello</p>
+  </>
+);
+export default app;
